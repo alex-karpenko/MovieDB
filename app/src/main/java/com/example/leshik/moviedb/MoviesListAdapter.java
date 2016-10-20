ade907ef5527f0c90b1ad0c149c65e7c47fcd276
Oleksii Karpenko <alexkarpenko@yahoo.com>
2016-10-20 11:26:15 +0300

    Update some comments

--- app/src/main/java/com/example/leshik/moviedb/MoviesListAdapter.java
+++ app/src/main/java/com/example/leshik/moviedb/MoviesListAdapter.java
@@ -12,7 +12,7 @@ import com.squareup.picasso.Picasso;
 import java.util.List;
 
 /**
- * Created by Leshik on 16.10.2016.
+ * Adapter class for handling list of moviews (posters) on main screen
  */
 
 public class MoviesListAdapter extends ArrayAdapter<MovieInfo> {
@@ -22,17 +22,20 @@ public class MoviesListAdapter extends ArrayAdapter<MovieInfo> {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        // get item object
         MovieInfo item = getItem(position);
 
+        // if it is a new view object, create it
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext())
                     .inflate(R.layout.movielist_item, parent, false);
         }
 
+        // get poster view and put poster image ito it using Picasso call
         ImageView itemView = (ImageView) convertView.findViewById(R.id.poster_image);
         Picasso.with(getContext())
                 .load(MovieUtils.basePosterUrl
-                        + "w185" // !!!
+                        + "w185" // TODO: we have to think to adopt width of image, mayby
                         + item.getPosterPath()).into(itemView);
         return convertView;
     }
