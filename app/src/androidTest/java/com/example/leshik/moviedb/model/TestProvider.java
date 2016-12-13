f1156326f6a2b1cca3e6efaa18d08bd120c27e3f
Oleksii Karpenko <alexkarpenko@yahoo.com>
2016-12-13 21:20:19 +0200

    Add single record deletion test for content provider

--- app/src/androidTest/java/com/example/leshik/moviedb/model/TestProvider.java
+++ app/src/androidTest/java/com/example/leshik/moviedb/model/TestProvider.java
@@ -2,6 +2,7 @@ package com.example.leshik.moviedb.model;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
@@ -14,6 +15,7 @@ import org.junit.runners.MethodSorters;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -41,6 +43,9 @@ public class TestProvider {
         ContentValues[] values = new ContentValues[moviesData.size()];
         int result = mContext.getContentResolver().bulkInsert(MoviesContract.Movies.CONTENT_URI, moviesData.toArray(values));
         assertEquals("Inserted wrong number of rows into movies table", values.length, result);
+        // double try, for testing dublicate checking mechanism
+        result = mContext.getContentResolver().bulkInsert(MoviesContract.Movies.CONTENT_URI, moviesData.toArray(values));
+        assertEquals("Inserted wrong number of rows into movies table", values.length, result);
 
         values = new ContentValues[popularData.size()];
         result = mContext.getContentResolver().bulkInsert(MoviesContract.Popular.CONTENT_URI, popularData.toArray(values));
@@ -56,20 +61,69 @@ public class TestProvider {
     }
 
     @Test
-    public void test02_delete() {
+    public void test03_delete() {
         test01_bulkInsert();
 
+        // Get one record from favorites table ...
+        Cursor c = mContext.getContentResolver().query(MoviesContract.Favorites.CONTENT_URI,
+                new String[]{MoviesContract.Favorites.COLUMN_NAME_SORT_ID},
+                null, null, null);
+        assertNotNull("Cursor is null after query favorites table.", c);
+        assertTrue("No next record in the cursor after query favorites table.", c.moveToNext());
+        int sort_id = c.getInt(c.getColumnIndex(MoviesContract.Favorites.COLUMN_NAME_SORT_ID));
+        c.close();
+        // ... and delete it ...
+        assertEquals("Can not delete one record from favorites table.",
+                mContext.getContentResolver().delete(MoviesContract.Favorites.buildUri(sort_id), null, null), 1);
+        // ... and delete all the rest.
         int result = mContext.getContentResolver().delete(MoviesContract.Favorites.CONTENT_URI, null, null);
-        assertTrue("Deleted wrong number of rows from favorites table", result > 0);
+        assertTrue("Deleted wrong number of rows from favorites table", result == TestDb.MOVIES_FAKE_DATA_ROWS / 2 - 1);
 
+
+        // Get one record from toprated table ...
+        c = mContext.getContentResolver().query(MoviesContract.TopRated.CONTENT_URI,
+                new String[]{MoviesContract.TopRated.COLUMN_NAME_SORT_ID},
+                null, null, null);
+        assertNotNull("Cursor is null after query toprated table.", c);
+        assertTrue("No next record in the cursor after query toprated table.", c.moveToNext());
+        sort_id = c.getInt(c.getColumnIndex(MoviesContract.TopRated.COLUMN_NAME_SORT_ID));
+        c.close();
+        // ... and delete it ...
+        assertEquals("Can not delete one record from toprated table.",
+                mContext.getContentResolver().delete(MoviesContract.TopRated.buildUri(sort_id), null, null), 1);
+        // ... and delete all the rest.
         result = mContext.getContentResolver().delete(MoviesContract.TopRated.CONTENT_URI, null, null);
-        assertTrue("Deleted wrong number of rows from toprated table", result > 0);
+        assertTrue("Deleted wrong number of rows from toprated table", result == TestDb.MOVIES_FAKE_DATA_ROWS - 1);
 
+        // Get one record from popular table ...
+        c = mContext.getContentResolver().query(MoviesContract.Popular.CONTENT_URI,
+                new String[]{MoviesContract.Popular.COLUMN_NAME_SORT_ID},
+                null, null, null);
+        assertNotNull("Cursor is null after query popular table.", c);
+        assertTrue("No next record in the cursor after query popular table.", c.moveToNext());
+        sort_id = c.getInt(c.getColumnIndex(MoviesContract.Popular.COLUMN_NAME_SORT_ID));
+        c.close();
+        // ... and delete it ...
+        assertEquals("Can not delete one record from popular table.",
+                mContext.getContentResolver().delete(MoviesContract.Popular.buildUri(sort_id), null, null), 1);
+        // ... and delete all the rest.
         result = mContext.getContentResolver().delete(MoviesContract.Popular.CONTENT_URI, null, null);
-        assertTrue("Deleted wrong number of rows from popular table", result > 0);
+        assertTrue("Deleted wrong number of rows from popular table", result == TestDb.MOVIES_FAKE_DATA_ROWS - 1);
 
+        // Get one record from movies table ...
+        c = mContext.getContentResolver().query(MoviesContract.Movies.CONTENT_URI,
+                new String[]{MoviesContract.Movies.COLUMN_NAME_MOVIE_ID},
+                null, null, null);
+        assertNotNull("Cursor is null after query movies table.", c);
+        assertTrue("No next record in the cursor after query movies table.", c.moveToNext());
+        int movie_id = c.getInt(c.getColumnIndex(MoviesContract.Movies.COLUMN_NAME_MOVIE_ID));
+        c.close();
+        // ... and delete it ...
+        assertEquals("Can not delete one record from movies table.",
+                mContext.getContentResolver().delete(MoviesContract.Movies.buildUri(movie_id), null, null), 1);
+        // ... and delete all the rest.
         result = mContext.getContentResolver().delete(MoviesContract.Movies.CONTENT_URI, null, null);
-        assertTrue("Deleted wrong number of rows from movies table", result > 0);
+        assertTrue("Deleted wrong number of rows from movies table", result == TestDb.MOVIES_FAKE_DATA_ROWS - 1);
     }
 
     private void deleteAllRecordsFromProvider() {
