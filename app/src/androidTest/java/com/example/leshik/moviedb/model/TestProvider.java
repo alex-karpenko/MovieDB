1fc2389503cb09062afb4e8a4ac9f95e1474d7dd
Oleksii Karpenko <alexkarpenko@yahoo.com>
2016-12-13 21:50:27 +0200

    Add tests for videos table in the TestProvider class

--- app/src/androidTest/java/com/example/leshik/moviedb/model/TestProvider.java
+++ app/src/androidTest/java/com/example/leshik/moviedb/model/TestProvider.java
@@ -39,6 +39,7 @@ public class TestProvider {
         List<ContentValues> topratedData = TestDb.topratedFakeData();
         List<ContentValues> popularData = TestDb.popularFakeData();
         List<ContentValues> favoritesData = TestDb.favoritesFakeData();
+        List<ContentValues> videosData = TestDb.videosFakeData();
 
         ContentValues[] values = new ContentValues[moviesData.size()];
         int result = mContext.getContentResolver().bulkInsert(MoviesContract.Movies.CONTENT_URI, moviesData.toArray(values));
@@ -58,14 +59,37 @@ public class TestProvider {
         values = new ContentValues[favoritesData.size()];
         result = mContext.getContentResolver().bulkInsert(MoviesContract.Favorites.CONTENT_URI, favoritesData.toArray(values));
         assertEquals("Inserted wrong number of rows into favorites table", values.length, result);
+
+        values = new ContentValues[videosData.size()];
+        result = mContext.getContentResolver().bulkInsert(MoviesContract.Videos.CONTENT_URI, videosData.toArray(values));
+        assertEquals("Inserted wrong number of rows into videos table", values.length, result);
     }
 
     @Test
     public void test03_delete() {
         test01_bulkInsert();
 
+        // Get one record from videos table ...
+        Cursor c = mContext.getContentResolver().query(MoviesContract.Videos.CONTENT_URI,
+                new String[]{MoviesContract.Videos.COLUMN_NAME_VIDEO_ID, MoviesContract.Videos.COLUMN_NAME_MOVIE_ID},
+                null, null, null);
+        assertNotNull("Cursor is null after query videos table.", c);
+        assertTrue("No next record in the cursor after query favorites table.", c.moveToNext());
+        String video_id = c.getString(c.getColumnIndex(MoviesContract.Videos.COLUMN_NAME_VIDEO_ID));
+        assertTrue("No next record in the cursor after query favorites table.", c.moveToNext());
+        int movie_id = c.getInt(c.getColumnIndex(MoviesContract.Videos.COLUMN_NAME_MOVIE_ID));
+        c.close();
+        // ... and delete it ...
+        assertEquals("Can not delete one record from videos table.",
+                mContext.getContentResolver().delete(MoviesContract.Videos.buildUri(video_id), null, null), 1);
+        assertEquals("Can not delete one record from videos table.",
+                mContext.getContentResolver().delete(MoviesContract.Videos.buildUri(movie_id), null, null), 1);
+        // ... and delete all the rest.
+        int result = mContext.getContentResolver().delete(MoviesContract.Videos.CONTENT_URI, null, null);
+        assertTrue("Deleted wrong number of rows from videos table", result == TestDb.MOVIES_FAKE_DATA_ROWS / 4 - 2);
+
         // Get one record from favorites table ...
-        Cursor c = mContext.getContentResolver().query(MoviesContract.Favorites.CONTENT_URI,
+        c = mContext.getContentResolver().query(MoviesContract.Favorites.CONTENT_URI,
                 new String[]{MoviesContract.Favorites.COLUMN_NAME_SORT_ID},
                 null, null, null);
         assertNotNull("Cursor is null after query favorites table.", c);
@@ -76,9 +100,8 @@ public class TestProvider {
         assertEquals("Can not delete one record from favorites table.",
                 mContext.getContentResolver().delete(MoviesContract.Favorites.buildUri(sort_id), null, null), 1);
         // ... and delete all the rest.
-        int result = mContext.getContentResolver().delete(MoviesContract.Favorites.CONTENT_URI, null, null);
+        result = mContext.getContentResolver().delete(MoviesContract.Favorites.CONTENT_URI, null, null);
         assertTrue("Deleted wrong number of rows from favorites table", result == TestDb.MOVIES_FAKE_DATA_ROWS / 2 - 1);
-
 
         // Get one record from toprated table ...
         c = mContext.getContentResolver().query(MoviesContract.TopRated.CONTENT_URI,
@@ -116,7 +139,7 @@ public class TestProvider {
                 null, null, null);
         assertNotNull("Cursor is null after query movies table.", c);
         assertTrue("No next record in the cursor after query movies table.", c.moveToNext());
-        int movie_id = c.getInt(c.getColumnIndex(MoviesContract.Movies.COLUMN_NAME_MOVIE_ID));
+        movie_id = c.getInt(c.getColumnIndex(MoviesContract.Movies.COLUMN_NAME_MOVIE_ID));
         c.close();
         // ... and delete it ...
         assertEquals("Can not delete one record from movies table.",
