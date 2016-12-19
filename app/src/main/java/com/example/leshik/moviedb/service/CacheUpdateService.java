3bb41c1aeda6eecd0e4f4aff2a13047691da1c6f
Oleksii Karpenko <alexkarpenko@yahoo.com>
2016-12-19 21:53:15 +0200

    Add CacheUpdateService skeleton

---
+++ app/src/main/java/com/example/leshik/moviedb/service/CacheUpdateService.java
@@ -0,0 +1,81 @@
+package com.example.leshik.moviedb.service;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p>
+ * helper methods.
+ */
+public class CacheUpdateService extends IntentService {
+    // IntentService can perform
+    private static final String ACTION_UPDATE_POPULAR = "com.example.leshik.moviedb.service.action.UPDATE_POPULAR";
+    private static final String ACTION_UPDATE_TOPRATED = "com.example.leshik.moviedb.service.action.UPDATE_TOPRATED";
+
+    private static final String EXTRA_PARAM_PAGE = "com.example.leshik.moviedb.service.extra.PAGE";
+
+    public CacheUpdateService() {
+        super("CacheUpdateService");
+    }
+
+    /**
+     * Starts this service to perform action Foo with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionUpdatePopular(Context context, int page) {
+        Intent intent = new Intent(context, CacheUpdateService.class);
+        intent.setAction(ACTION_UPDATE_POPULAR);
+        intent.putExtra(EXTRA_PARAM_PAGE, page);
+        context.startService(intent);
+    }
+
+    /**
+     * Starts this service to perform action Baz with the given parameters. If
+     * the service is already performing a task this action will be queued.
+     *
+     * @see IntentService
+     */
+    public static void startActionUpdateToprated(Context context, int page) {
+        Intent intent = new Intent(context, CacheUpdateService.class);
+        intent.setAction(ACTION_UPDATE_TOPRATED);
+        intent.putExtra(EXTRA_PARAM_PAGE, page);
+        context.startService(intent);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_UPDATE_POPULAR.equals(action)) {
+                final int page = intent.getIntExtra(EXTRA_PARAM_PAGE, -1);
+                handleActionUpdatePopular(page);
+            } else if (ACTION_UPDATE_TOPRATED.equals(action)) {
+                final int page = intent.getIntExtra(EXTRA_PARAM_PAGE, -1);
+                handleActionUpdateToprated(page);
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionUpdatePopular(int page) {
+        // TODO: Handle action UpdatePopular
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void handleActionUpdateToprated(int page) {
+        // TODO: Handle action UpdateToprated
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
