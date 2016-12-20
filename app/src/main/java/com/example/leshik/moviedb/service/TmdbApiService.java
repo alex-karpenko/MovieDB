13a6f7ed032cbcadd5decf5dd2041a757abc8c0a
Oleksii Karpenko <alexkarpenko@yahoo.com>
2016-12-20 21:05:31 +0200

    Add import expressions

--- app/src/main/java/com/example/leshik/moviedb/service/TmdbApiService.java
+++ app/src/main/java/com/example/leshik/moviedb/service/TmdbApiService.java
@@ -1,5 +1,10 @@
 package com.example.leshik.moviedb.service;
 
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
 public interface TmdbApiService {
   @GET("configuration")
   Call<TmdbConfiguration> getConfiguration(@Query("api_key") String apiKey);
