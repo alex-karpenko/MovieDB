db24515c5096355fb505681b387107dc5206e458
Oleksii Karpenko <alexkarpenko@yahoo.com>
2016-12-21 22:50:53 +0200

    Add getters for getting ContentValue arrays from page class

--- app/src/main/java/com/example/leshik/moviedb/service/TmdbListPage.java
+++ app/src/main/java/com/example/leshik/moviedb/service/TmdbListPage.java
@@ -4,6 +4,9 @@ package com.example.leshik.moviedb.service;
  * Created by Leshik on 19.12.2016.
  */
 
+import android.content.ContentValues;
+
+import com.example.leshik.moviedb.model.MoviesContract;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
@@ -71,4 +74,65 @@ public class TmdbListPage {
     @Expose
     public Integer totalPages;
 
+    public ContentValues[] getPopularContentValues() {
+        if (results == null) return null;
+
+        int resultsSize = results.size();
+        if (resultsSize == 0) return null;
+
+        int basePage = (page - 1) * resultsSize;
+        ContentValues[] returnValues = new ContentValues[resultsSize];
+
+        for (int i = 0; i < resultsSize; i++) {
+            returnValues[i] = new ContentValues();
+            returnValues[i].put(MoviesContract.Popular.COLUMN_NAME_SORT_ID, basePage + i + 1);
+            returnValues[i].put(MoviesContract.Popular.COLUMN_NAME_MOVIE_ID, results.get(i).id);
+        }
+
+        return returnValues;
+    }
+
+    public ContentValues[] getTopratedContentValues() {
+        if (results == null) return null;
+
+        int resultsSize = results.size();
+        if (resultsSize == 0) return null;
+
+        int basePage = (page - 1) * resultsSize;
+        ContentValues[] returnValues = new ContentValues[results.size()];
+
+        for (int i = 0; i < resultsSize; i++) {
+            returnValues[i] = new ContentValues();
+            returnValues[i].put(MoviesContract.Toprated.COLUMN_NAME_SORT_ID, basePage + i + 1);
+            returnValues[i].put(MoviesContract.Toprated.COLUMN_NAME_MOVIE_ID, results.get(i).id);
+        }
+
+        return returnValues;
+    }
+
+    public ContentValues[] getMoviesContentValues() {
+        if (results == null) return null;
+
+        int resultsSize = results.size();
+        if (resultsSize == 0) return null;
+
+        int basePage = (page - 1) * resultsSize;
+        ContentValues[] returnValues = new ContentValues[results.size()];
+
+        for (int i = 0; i < resultsSize; i++) {
+            Result res = results.get(i);
+            returnValues[i] = new ContentValues();
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_MOVIE_ID, res.id);
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_ORIGINAL_TITLE, res.originalTitle);
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_OVERVIEW, res.overview);
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_RELEASE_DATE, res.releaseDate);
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_VOTE_AVERAGE, res.voteAverage);
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_POPULARITY, res.popularity);
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_POSTER_PATH, res.posterPath);
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_ADULT, res.adult);
+            returnValues[i].put(MoviesContract.Movies.COLUMN_NAME_VIDEO, res.video);
+        }
+
+        return returnValues;
+    }
 }
