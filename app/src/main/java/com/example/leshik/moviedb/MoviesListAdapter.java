87295ce2a6506140a18e1168418eac157f1f18b7
Oleksii Karpenko <alexkarpenko@yahoo.com>
2016-12-24 23:26:33 +0200

    ListAdapter: add view holder for store movie_id

--- app/src/main/java/com/example/leshik/moviedb/MoviesListAdapter.java
+++ app/src/main/java/com/example/leshik/moviedb/MoviesListAdapter.java
@@ -15,24 +15,40 @@ import com.squareup.picasso.Picasso;
  * Created by Leshik on 24.12.2016.
  */
 
-public class MoviesListAdapter extends CursorAdapter {
-    public MoviesListAdapter(Context context, Cursor c, int flags) {
+class MoviesListAdapter extends CursorAdapter {
+    MoviesListAdapter(Context context, Cursor c, int flags) {
         super(context, c, flags);
+    }
+
+    static class ViewHolder {
+        final ImageView posterImage;
+        int movie_id;
+
+        ViewHolder(View view, int movie_id) {
+            posterImage = (ImageView) view.findViewById(R.id.poster_image);
+            this.movie_id = movie_id;
+        }
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
         View view = LayoutInflater.from(context).inflate(R.layout.movielist_item, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view, cursor.getInt(MoviesContract.SHORT_PROJECTION_INDEX_MOVIE_ID));
+        view.setTag(viewHolder);
+
         return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        ImageView image = (ImageView) view.findViewById(R.id.poster_image);
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+
+        viewHolder.movie_id = cursor.getInt(MoviesContract.SHORT_PROJECTION_INDEX_MOVIE_ID);
+
         Picasso.with(context)
                 .load(MovieUtils.basePosterUrl
                         + "w185" // TODO: we have to think to adopt width of image, mayby
                         + cursor.getString(MoviesContract.SHORT_PROJECTION_INDEX_POSTER_PATH))
-                .into(image);
+                .into(viewHolder.posterImage);
     }
 }
