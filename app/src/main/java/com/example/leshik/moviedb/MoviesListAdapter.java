147552fe6d51ee6c548c5ecca96781bb6f762e6e
Oleksii Karpenko <alexkarpenko@yahoo.com>
2016-12-24 22:48:45 +0200

    Add cursor adapter

--- app/src/main/java/com/example/leshik/moviedb/MoviesListAdapter.java
+++ app/src/main/java/com/example/leshik/moviedb/MoviesListAdapter.java
@@ -1,42 +1,38 @@
 package com.example.leshik.moviedb;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 
+import com.example.leshik.moviedb.model.MoviesContract;
 import com.squareup.picasso.Picasso;
 
-import java.util.List;
-
 /**
- * Adapter class for handling list of moviews (posters) on main screen
+ * Created by Leshik on 24.12.2016.
  */
 
-public class MoviesListAdapter extends ArrayAdapter<MovieInfo> {
-    public MoviesListAdapter(Context context, List<MovieInfo> objects) {
-        super(context, 0, objects);
+public class MoviesListAdapter extends CursorAdapter {
+    public MoviesListAdapter(Context context, Cursor c, int flags) {
+        super(context, c, flags);
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        // get item object
-        MovieInfo item = getItem(position);
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view = LayoutInflater.from(context).inflate(R.layout.movielist_item, parent, false);
+        return view;
+    }
 
-        // if it is a new view object, create it
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext())
-                    .inflate(R.layout.movielist_item, parent, false);
-        }
-
-        // get poster view and put poster image ito it using Picasso call
-        ImageView itemView = (ImageView) convertView.findViewById(R.id.poster_image);
-        Picasso.with(getContext())
+    @Override
+    public void bindView(View view, Context context, Cursor cursor) {
+        ImageView image = (ImageView) view.findViewById(R.id.poster_image);
+        Picasso.with(context)
                 .load(MovieUtils.basePosterUrl
                         + "w185" // TODO: we have to think to adopt width of image, mayby
-                        + item.getPosterPath()).into(itemView);
-        return convertView;
+                        + cursor.getString(MoviesContract.SHORT_PROJECTION_INDEX_POSTER_PATH))
+                .into(image);
     }
 }
