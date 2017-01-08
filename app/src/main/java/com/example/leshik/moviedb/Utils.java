45b710a968fb224ab0314df82a434fbde41d2320
Oleksii Karpenko <alexkarpenko@yahoo.com>
2017-01-08 13:48:35 +0200

    Fix number of columns on small screen

--- app/src/main/java/com/example/leshik/moviedb/Utils.java
+++ app/src/main/java/com/example/leshik/moviedb/Utils.java
@@ -36,6 +36,7 @@ public final class Utils {
         DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
         float dpWidth = displayMetrics.widthPixels / displayMetrics.density;
         int noOfColumns = (int) (dpWidth / 180);
+        if (noOfColumns == 1) noOfColumns = 2;
         return noOfColumns;
     }
 }
