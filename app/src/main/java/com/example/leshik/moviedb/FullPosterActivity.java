6ae465d710681b34db67d1739c5077195e28cbd7
Oleksii Karpenko <alexkarpenko@yahoo.com>
2017-01-16 22:25:03 +0200

    Add toolbar to full poster view

--- app/src/main/java/com/example/leshik/moviedb/FullPosterActivity.java
+++ app/src/main/java/com/example/leshik/moviedb/FullPosterActivity.java
@@ -7,6 +7,8 @@ import android.os.Handler;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
@@ -15,8 +17,8 @@ import android.view.View;
  * status bar and navigation/system bar) with user interaction.
  */
 public class FullPosterActivity extends AppCompatActivity {
-
     public static final String ARG_POSTER_NAME = "POSTER_NAME";
+    public static final String ARG_MOVIE_ID = "MOVIE_ID";
 
     /**
      * Some older devices needs a small delay between UI widget updates
@@ -65,14 +67,18 @@ public class FullPosterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_full_poster);
+
+        Toolbar mToolbar = (Toolbar) findViewById(R.id.poster_toolbar);
+        setSupportActionBar(mToolbar);
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
         mVisible = true;
-        mContentView = findViewById(R.id.fullscreen_content);
 
+        mContentView = findViewById(R.id.fullscreen_content);
 
         // Set up the user interaction to manually show or hide the system UI.
         mContentView.setOnClickListener(new View.OnClickListener() {
@@ -85,11 +91,18 @@ public class FullPosterActivity extends AppCompatActivity {
         // get poster image name from intent
         Intent intent = getIntent();
         String posterName = intent.getStringExtra(ARG_POSTER_NAME);
+        int movieId = intent.getIntExtra(ARG_MOVIE_ID, -1);
         // Inflate new fragment full screen poster image
-        FullPosterFragment fragment = FullPosterFragment.newInstance(posterName);
+        FullPosterFragment fragment = FullPosterFragment.newInstance(movieId, posterName);
         getSupportFragmentManager().beginTransaction()
                 .add(R.id.fullscreen_content, fragment)
                 .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.poster, menu);
+        return true;
     }
 
     @Override
