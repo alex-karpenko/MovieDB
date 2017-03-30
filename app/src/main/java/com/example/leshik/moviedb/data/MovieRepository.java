a904a9a7aceda46809acc8fdf9329cd8608e112c
Oleksii Karpenko <alexkarpenko@yahoo.com>
2017-03-30 22:21:31 +0300

    Change forceRefresh method

--- app/src/main/java/com/example/leshik/moviedb/data/MovieRepository.java
+++ app/src/main/java/com/example/leshik/moviedb/data/MovieRepository.java
@@ -1,29 +1,20 @@
 package com.example.leshik.moviedb.data;
 
 import android.content.Context;
-import android.util.Log;
 
 import com.example.leshik.moviedb.data.interfaces.CacheStorage;
 import com.example.leshik.moviedb.data.interfaces.MovieInteractor;
 import com.example.leshik.moviedb.data.interfaces.NetworkDataSource;
 import com.example.leshik.moviedb.data.interfaces.PreferenceInterface;
 import com.example.leshik.moviedb.data.model.Movie;
-import com.example.leshik.moviedb.data.model.Review;
-import com.example.leshik.moviedb.data.model.Video;
 import com.example.leshik.moviedb.utils.EventsUtils;
-import com.example.leshik.moviedb.utils.NetworkUtils;
 
 import java.util.Calendar;
-import java.util.List;
 
 import io.reactivex.Observable;
 import io.reactivex.ObservableSource;
 import io.reactivex.annotations.NonNull;
-import io.reactivex.functions.BiFunction;
-import io.reactivex.functions.Consumer;
 import io.reactivex.functions.Function;
-import io.reactivex.functions.Predicate;
-import io.reactivex.schedulers.Schedulers;
 
 /**
  * Created by Leshik on 17.02.2017.
@@ -34,19 +25,17 @@ public class MovieRepository implements MovieInteractor {
     private final NetworkDataSource networkDataSource;
     private final CacheStorage cacheStorage;
     private final PreferenceInterface prefStorage;
-    private final Context context;
 
     public MovieRepository(Context context) {
         prefStorage = PreferenceStorage.getInstance(context);
         networkDataSource = new TmdbNetworkDataSource(prefStorage.getBaseApiUrl());
         cacheStorage = new RealmCacheStorage(context);
-        this.context = context;
     }
 
     @Override
     public Observable<Movie> getMovie(final long movieId) {
-        final Observable<Movie> movieFromCache = cacheStorage.getMovieObservable(movieId);
-        final Observable<Movie> movieFromNetwork = buildMovieFromNetworkObservable(movieId);
+        final Observable<Movie> movieFromCache = cacheStorage.getMovie(movieId);
+        final Observable<Movie> movieFromNetwork = networkDataSource.readMovieFull(movieId);
 
 
         Observable<Movie> returnMovie = movieFromCache.flatMap(new Function<Movie, ObservableSource<Movie>>() {
@@ -100,48 +89,30 @@ public class MovieRepository implements MovieInteractor {
 
     @Override
     public boolean forceRefresh(long movieId) {
-        if (NetworkUtils.isNetworkConnected(context)) {
-            // TODO: 04.03.2017 Must be checked for memory leaks
-            buildMovieFromNetworkObservable(movieId)
-                    .subscribeOn(Schedulers.io())
-                    .observeOn(Schedulers.io())
-                    .subscribe(new Consumer<Movie>() {
+        // Simply set last update time to 0
+        // after this we have to unsubscribe from movie and subscribe again
+        // TODO: 3/30/17 May be delete this method???
+        try {
+            cacheStorage.getMovie(movieId)
+                    .flatMap(new Function<Movie, ObservableSource<Movie>>() {
                         @Override
-                        public void accept(Movie movie) throws Exception {
-                            Log.i(TAG, "forceRefresh: +");
-                            cacheStorage.updateOrInsertMovie(movie);
+                        public ObservableSource<Movie> apply(@NonNull Movie movie) throws Exception {
+                            movie.setLastUpdate(0L);
+                            return Observable.just(movie);
                         }
-                    });
-        } else {
+                    })
+                    .map(new Function<Movie, Long>() {
+                        @Override
+                        public Long apply(@NonNull Movie movie) throws Exception {
+                            return cacheStorage.updateOrInsertMovie(movie);
+                        }
+                    })
+                    .blockingSubscribe();
+            return true;
+        } catch (Exception e) {
             EventsUtils.postEvent(EventsUtils.EventType.NetworkUnavailable);
         }
 
         return false;
-    }
-
-    private Observable<Movie> buildMovieFromNetworkObservable(long movieId) {
-        return networkDataSource.readMovie(movieId)
-                .zipWith(networkDataSource.readVideoList(movieId),
-                        new BiFunction<Movie, List<Video>, Movie>() {
-                            @Override
-                            public Movie apply(Movie movie, List<Video> videos) throws Exception {
-                                movie.setVideos(videos);
-                                return movie;
-                            }
-                        })
-                .zipWith(networkDataSource.readReviewList(movieId),
-                        new BiFunction<Movie, List<Review>, Movie>() {
-                            @Override
-                            public Movie apply(Movie movie, List<Review> reviews) throws Exception {
-                                movie.setReviews(reviews);
-                                return movie;
-                            }
-                        })
-                .filter(new Predicate<Movie>() {
-                    @Override
-                    public boolean test(@NonNull Movie movie) throws Exception {
-                        return !isEmpty(movie);
-                    }
-                });
     }
 }
